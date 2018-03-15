731df490eacf6ab24a023c9212d3a7fa1f6e1016
sqiu3255 <32362370+sqiu3255@users.noreply.github.com>
2018-03-15 09:27:51 -0400

    Update Deck.java

--- Lab4.1SQ/src/Deck.java
+++ Lab4.1SQ/src/Deck.java
@@ -1,4 +1,16 @@
 
 public class Deck {
 
+	//Deck Constructor
+	public Deck (String[] rank, String[] suit, int[] value)
+	{
+		for (int i = 0; i < rank.length; i++)
+		{
+			for (int j = 0; j < suit.length; j++)
+			{
+				unDealt.add(new Card(rank[i], suit[j], value[i]));
+			}
+		}
+	}
+	
 }
